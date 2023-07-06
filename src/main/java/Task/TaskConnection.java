89129d5301cccabb29dc08918ed4773fd630d96b
Yamatosilvestor <akulbekmama@gmail.com>
2023-07-06 19:31:31 +0600

    sdsfdgfhg

---
+++ src/main/java/Task/TaskConnection.java
@@ -0,0 +1,14 @@
+package Task;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TaskConnection {
+    String url = "jdbc:postgresql://localhost:5432/books";
+    String user = "postgres";
+    String password = "postgres";
+    public static void addUser(User user) {
+        try ()
+    }
+}
